4db05921ee9f99a148d140ad94d2e20b52a2a8b4
Canh Nguyen <nguyenduccanh2501@gmail.com>
2019-06-10 10:46:40 +0700

    create splash screen

--- app/src/main/java/com/sun/moviedb_45/ui/MainActivity.java
+++ app/src/main/java/com/sun/moviedb_45/ui/MainActivity.java
@@ -1,7 +1,11 @@
-package com.sun.moviedb_45;
+package com.sun.moviedb_45.ui;
 
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.sun.moviedb_45.R;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public static Intent getIntent(Context context){
+        return new Intent(context, MainActivity.class);
     }
 }
