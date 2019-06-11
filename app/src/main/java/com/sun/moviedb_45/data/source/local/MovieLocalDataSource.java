ca994518076624954bb600488d15f10f22369e96
Canh Nguyen <nguyenduccanh2501@gmail.com>
2019-06-11 13:35:38 +0700

    fetch and display data

--- app/src/main/java/com/sun/moviedb_45/data/source/local/MovieLocalDataSource.java
+++ app/src/main/java/com/sun/moviedb_45/data/source/local/MovieLocalDataSource.java
@@ -1,6 +1,16 @@
 package com.sun.moviedb_45.data.source.local;
 
+import android.content.Context;
+
 import com.sun.moviedb_45.data.source.MovieDataSource;
 
 public class MovieLocalDataSource implements MovieDataSource.Local {
+    private static MovieLocalDataSource sInstance;
+
+    public static MovieLocalDataSource getInstance(Context context){
+        if (sInstance == null){
+            sInstance = new MovieLocalDataSource();
+        }
+        return sInstance;
+    }
 }
--- app/src/main/java/com/sun/moviedb_45/ui/home/HomeNavigator.java
+++ app/src/main/java/com/sun/moviedb_45/ui/home/HomeNavigator.java
@@ -1,4 +1,8 @@
 package com.sun.moviedb_45.ui.home;
 
-public class HomeNavigator {
+import com.sun.moviedb_45.data.model.Movie;
+
+public interface HomeNavigator {
+    void showMovie(String categoryKey, String categoryName);
+    void showMovieDetail(Movie movie);
 }
